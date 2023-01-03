f8d882434ef19255c723a3207581d822dba8541b
amilz.sol <85324096+amilz@users.noreply.github.com>
2023-01-02 17:50:03 -0800

    vercel errors

--- app/components/HomeView.tsx
+++ app/components/HomeView.tsx
@@ -39,8 +39,8 @@ export const HomeView: FC = ({ }) => {
                 <title>The Bonk Fire</title>
                 <meta name="description" content="BuildSpace Core 2022 Demo Project" />
                 <link rel="icon" href="/favicon.ico" />
-                <link rel="preconnect" href="https://fonts.googleapis.com" />
-                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='' />
+                 {/* <link rel="preconnect" href="https://fonts.googleapis.com" />
+               <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='' /> */}
                 <link href="https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap" rel="stylesheet" />
             </Head>
             <main className={styles.main}>
