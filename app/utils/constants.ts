58cd67d87cb5d5cfd1c3b9db26730ce30660bd32
amilz.sol <85324096+amilz@users.noreply.github.com>
2023-01-02 22:04:08 -0800

    type rpc

--- app/utils/constants.ts
+++ app/utils/constants.ts
@@ -2,5 +2,5 @@ import { clusterApiUrl, PublicKey } from "@solana/web3.js"
 
 export const PROGRAM_ID = new PublicKey('FiRESpaNzgYUiba5vkb44CZJLZjrux1AUECdfwPRsNkg');
 export const BONK_MINT = new PublicKey('DezXAZ8z7PnrnRJjz3wXBoRgixCa6xjnB7YaB1pPB263');
-export const SOLANA_RPC = process.env.NEXT_PUBLIC_RPC;
+export const SOLANA_RPC: string = process.env.NEXT_PUBLIC_RPC as string;
 export const FUNGIBLE_USER_SEED = 'fungibleuser';
