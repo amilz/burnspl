910618354f9b05af8b9440f3285477cf45f903b0
amilz.sol <85324096+amilz@users.noreply.github.com>
2023-01-08 04:55:39 -0800

    Format code - run prettier

--- app/components/HomeView.tsx
+++ app/components/HomeView.tsx
@@ -41,10 +41,10 @@ export const HomeView: FC = ({ }) => {
                 <title>The Bonk Fire</title>
                 <meta name="description" content="Track BONK Burns" />
                 <link rel="icon" href="/favicon.ico" />
-                <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
-                <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
-                <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
-                <link rel="manifest" href="/site.webmanifest"/>
+                <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+                <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+                <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+                <link rel="manifest" href="/site.webmanifest" />
                 <link href="https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap" rel="stylesheet" />
             </Head>
             <main className={styles.main}>
