6690f88ad80bf1f0ec325be2e1adfefc53819767
amilz.sol <85324096+amilz@users.noreply.github.com>
2023-01-14 14:32:28 -0800

    initial commit
    known issue is a blocker for pushing this to production. will re-evaluate if we scale.
    https://github.com/amilz/burnspl/issues/12

--- app/utils/instructions.ts
+++ app/utils/instructions.ts
@@ -1,20 +1,20 @@
 import { PublicKey, SystemProgram, SYSVAR_RENT_PUBKEY, TransactionInstruction } from "@solana/web3.js"
 import { BurnBoardIdl, BurnScore } from "./idl"
-import { Program,  } from "@project-serum/anchor"
+import { Program, } from "@project-serum/anchor"
 import { ASSOCIATED_TOKEN_PROGRAM_ID, getAssociatedTokenAddress, TOKEN_PROGRAM_ID } from "@solana/spl-token"
 import { BN } from "bn.js"
 import { FUNGIBLE_USER_SEED } from "./constants"
-  
+
 export async function createInitBurnAccountIx(
     burnBoardProgram: Program<BurnBoardIdl>,
     userPubkey: PublicKey,
     mintPubkey: PublicKey,
-    userName: string, 
+    userName: string,
 ): Promise<TransactionInstruction> {
     const [BURN_SCORE] = await PublicKey.findProgramAddressSync(
         [Buffer.from(FUNGIBLE_USER_SEED), mintPubkey.toBytes(), userPubkey.toBytes()],
         burnBoardProgram.programId
-      );
+    );
     const tx = await burnBoardProgram.methods
         .initFungibleUser(userName)
         .accountsStrict({
@@ -25,7 +25,7 @@ export async function createInitBurnAccountIx(
         })
         .instruction()
 
-    return tx;  
+    return tx;
 
 }
 
@@ -33,13 +33,13 @@ export async function createBurnIx(
     burnBoardProgram: Program<BurnBoardIdl>,
     userPubkey: PublicKey,
     mintPubkey: PublicKey,
-    amountToBurn: number, 
+    amountToBurn: number,
 ): Promise<TransactionInstruction> {
     const [BURN_SCORE] = await PublicKey.findProgramAddressSync(
         [Buffer.from(FUNGIBLE_USER_SEED), mintPubkey.toBytes(), userPubkey.toBytes()],
         burnBoardProgram.programId
-      );
-    const TOKEN_ACCOUNT = await getAssociatedTokenAddress(mintPubkey,userPubkey);
+    );
+    const TOKEN_ACCOUNT = await getAssociatedTokenAddress(mintPubkey, userPubkey);
 
     const tx = await burnBoardProgram.methods
         .burnFungible(new BN(amountToBurn))
@@ -52,19 +52,48 @@ export async function createBurnIx(
         })
         .instruction()
 
-    return tx;  
+    return tx;
 
 }
-  
+
+
+
 export async function fetchBurnAcctsByToken(
     burnBoardProgram: Program<BurnBoardIdl>,
-    mint?:PublicKey,
-    ) {
+    mint: PublicKey,
+) {
+    const allBurnAccts = await burnBoardProgram.account.burnScore.all(
+        [{
+            memcmp: {
+                offset: 8 // Discriminator
+                    + 8 // burned_tokens
+                    + 8 // num_burns
+                    + 32 // pyro_key
+                    + 4 + 8, // user_name
+                bytes: mint.toBase58(),
+            }
+        }]
+        //ref: https://lorisleiva.com/create-a-solana-dapp-from-scratch/fetching-tweets-in-the-frontend
+    );
+        console.log(allBurnAccts);
+    if (!mint) return allBurnAccts;
+    return allBurnAccts.filter(acct => acct.account.mint.toBase58() == mint.toBase58());
+}
+
+
+
+
+/**
+ * DEPRECATING 1/14 b/c added memcmp filters. keeping here temporarily in case we need to revert back. 
+ * @param burnBoardProgram 
+ * @param mint 
+ * @returns 
+ */
+export async function fetchBurnAcctsByTokenArchive(
+    burnBoardProgram: Program<BurnBoardIdl>,
+    mint?: PublicKey,
+) {
     const allBurnAccts = await burnBoardProgram.account.burnScore.all();
     if (!mint) return allBurnAccts;
-    return allBurnAccts.filter(acct=> acct.account.mint.toBase58() == mint.toBase58());
+    return allBurnAccts.filter(acct => acct.account.mint.toBase58() == mint.toBase58());
 }
-  
-
-  
-
